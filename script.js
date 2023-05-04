b561f8e5c8ecfdf118120f34cb7c7be1692aed35
Godwin Duliente <rualgodwin@gmail.com>
2023-05-04 23:11:22 +0800

    updated js filter

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-const userBtn = document.querySelector("#acc-btn");
-const exitBtn = document.querySelector("#exit-btn");
-const memberBtn = document.querySelector("#member-btn");
-const sellerBtn = document.querySelector("#seller-btn");
-const cateBtn = document.querySelector("#cate-btn");
+const userBtn = document.querySelector('#acc-btn');
+const userPopBtn = document.querySelector('#exit-btn');
+const memberBtn = document.querySelector('#member-btn');
+const sellerBtn = document.querySelector('#seller-btn');
+const cateBtn = document.querySelector('#cate-btn');
+const priceBtn = document.querySelector('#price-btn');
+const colorBtn = document.querySelector('#color-btn');
+const sizeBtn = document.querySelector('#size-btn');
+const memberExitBtn = document.querySelector('#member-exit');
+const registerExitBtn = document.querySelector('#register-exit');
+const sellerExitBtn = document.querySelector('#seller-exit');
 
-const loginModal = document.querySelector(".user-pop")
-const memberModal = document.querySelector(".login");
-const sellerModal = document.querySelector(".seller");
-const cateModal = document.querySelector(".categories");
+const loginModal = document.querySelector('.user-pop')
+const memberModal = document.querySelector('.login');
+const sellerModal = document.querySelector('.seller');
+const registerModal = document.querySelector('.register');
+const cateModal = document.querySelector('.categories');
+const priceModal = document.querySelector('.price-filter');
+const colorModal = document.querySelector('.colors');
+const sizeModal = document.querySelector('.sizes');
+const toggleColor = document.querySelector('toggle-color');
 
+const priceToggle = document.querySelector('#price-toggle');
+const colorToggle = document.querySelector('#color-toggle');
+const sizeToggle = document.querySelector('#size-toggle');
+const cateToggle = document.querySelector('#cate-toggle');
+
+// Popup Modals
 userBtn.onclick = () => {
   loginModal.classList.toggle('active');
   memberModal.classList.remove('active');
@@ -31,13 +48,42 @@ sellerBtn.onclick = () => {
 }
 
 cateBtn.onclick = () => {
+  cateToggle.classList.toggle('fa-angle-down');
   loginModal.classList.remove('active');
   memberModal.classList.remove('active');
   sellerModal.classList.remove('active');
   cateModal.classList.toggle('active');
+  toggleColor.classList.toggle('active');
 }
 
-exitBtn.onclick = () => {
+userPopBtn.onclick = () => {
   loginModal.classList.remove('active');
 }
 
+memberExitBtn.onclick = () => {
+  memberModal.classList.remove('active');
+}
+
+sellerExitBtn.onclick = () => {
+  sellerModal.classList.remove('active');
+}
+
+registerExitBtn.onclick = () => {
+  registerModal.classList.remove('active');
+}
+
+// Filters
+priceBtn.onclick = () => {
+  priceToggle.classList.toggle('fa-angle-up');
+  priceModal.classList.toggle('active');
+}
+
+colorBtn.onclick = () => {
+  colorToggle.classList.toggle('fa-angle-up');
+  colorModal.classList.toggle('active');
+}
+
+sizeBtn.onclick = () => {
+  sizeToggle.classList.toggle('fa-angle-up');
+  sizeModal.classList.toggle('active');
+}
