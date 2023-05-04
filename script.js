6cb560e37b95e4b411a61166e232a53951b23c00
Godwin Duliente <rualgodwin@gmail.com>
2023-05-04 08:00:05 +0800

    updated js

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
 const userBtn = document.querySelector("#acc-btn");
+const exitBtn = document.querySelector("#exit-btn");
+const memberBtn = document.querySelector("#member-btn");
+const sellerBtn = document.querySelector("#seller-btn");
+const cateBtn = document.querySelector("#cate-btn");
 
 const loginModal = document.querySelector(".user-pop")
+const memberModal = document.querySelector(".login");
+const sellerModal = document.querySelector(".seller");
+const cateModal = document.querySelector(".categories");
 
 userBtn.onclick = () => {
   loginModal.classList.toggle('active');
+  memberModal.classList.remove('active');
+  sellerModal.classList.remove('active');
+  cateModal.classList.remove('active');
+}
+
+memberBtn.onclick = () => {
+  loginModal.classList.remove('active');
+  memberModal.classList.toggle('active');
+  sellerModal.classList.remove('active');
+  cateModal.classList.remove('active');
+}
+
+sellerBtn.onclick = () => {
+  loginModal.classList.remove('active');
+  memberModal.classList.remove('active');
+  sellerModal.classList.toggle('active');
+  cateModal.classList.remove('active');
+}
+
+cateBtn.onclick = () => {
+  loginModal.classList.remove('active');
+  memberModal.classList.remove('active');
+  sellerModal.classList.remove('active');
+  cateModal.classList.toggle('active');
+}
+
+exitBtn.onclick = () => {
+  loginModal.classList.remove('active');
 }
 
