05e65e90d71234362563f1382db16be660b7d4de
Godwin Duliente <rualgodwin@gmail.com>
2023-05-05 09:15:29 +0800

    js update

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-const userBtn = document.querySelector('#acc-btn');
-const userPopBtn = document.querySelector('#exit-btn');
-const memberBtn = document.querySelector('#member-btn');
-const sellerBtn = document.querySelector('#seller-btn');
-const cateBtn = document.querySelector('#cate-btn');
-const priceBtn = document.querySelector('#price-btn');
-const colorBtn = document.querySelector('#color-btn');
-const sizeBtn = document.querySelector('#size-btn');
-const memberExitBtn = document.querySelector('#member-exit');
-const registerExitBtn = document.querySelector('#register-exit');
-const sellerExitBtn = document.querySelector('#seller-exit');
+// Variables
+const accountButton = document.querySelector('#acc-btn');
+const exitButton = document.querySelector('#exit-btn');
+const memberButton = document.querySelector('#member-btn');
+const sellerButton = document.querySelector('#seller-btn');
+const ratingButton = document.querySelector('#rating-btn');
+const categoriesButton = document.querySelector('#cate-btn');
+const priceButton = document.querySelector('#price-btn');
+const colorButton = document.querySelector('#color-btn');
+const sizeButton = document.querySelector('#size-btn');
+const memberExitButton = document.querySelector('#member-exit');
+const registerExitButton = document.querySelector('#register-exit');
+const sellerExitButton = document.querySelector('#seller-exit');
+const ratingExitButton = document.querySelector('#rating-exit');
 
-const loginModal = document.querySelector('.user-pop')
+const loginModal = document.querySelector('.user-pop');
 const memberModal = document.querySelector('.login');
 const sellerModal = document.querySelector('.seller');
 const registerModal = document.querySelector('.register');
-const cateModal = document.querySelector('.categories');
+const ratingModal = document.querySelector('.rating');
+const categoriesModal = document.querySelector('.categories');
 const priceModal = document.querySelector('.price-filter');
 const colorModal = document.querySelector('.colors');
 const sizeModal = document.querySelector('.sizes');
@@ -23,67 +27,86 @@ const toggleColor = document.querySelector('toggle-color');
 const priceToggle = document.querySelector('#price-toggle');
 const colorToggle = document.querySelector('#color-toggle');
 const sizeToggle = document.querySelector('#size-toggle');
-const cateToggle = document.querySelector('#cate-toggle');
+const categoriesToggle = document.querySelector('#cate-toggle');
+
+// Filters
+priceButton.addEventListener('click', () => {
+  priceToggle.classList.toggle('fa-angle-up');
+  priceModal.classList.toggle('active');
+});
+
+colorButton.addEventListener('click', () => {
+  colorToggle.classList.toggle('fa-angle-up');
+  colorModal.classList.toggle('active');
+});
+
+sizeButton.addEventListener('click', () => {
+  sizeToggle.classList.toggle('fa-angle-up');
+  sizeModal.classList.toggle('active');
+});
+
+// Exit Buttons
+exitButton.addEventListener('click', () => {
+  loginModal.classList.remove('active');
+});
+
+memberExitButton.addEventListener('click', () => {
+  memberModal.classList.remove('active');
+});
+
+sellerExitButton.addEventListener('click', () => {
+  sellerModal.classList.remove('active');
+});
+
+registerExitButton.addEventListener('click', () => {
+  registerModal.classList.remove('active');
+});
+
+ratingExitButton.addEventListener('click', () => {
+  ratingModal.classList.remove('active');
+});
+
+
 
 // Popup Modals
-userBtn.onclick = () => {
+accountButton.addEventListener('click', () => {
   loginModal.classList.toggle('active');
   memberModal.classList.remove('active');
   sellerModal.classList.remove('active');
-  cateModal.classList.remove('active');
-}
+  categoriesModal.classList.remove('active');
+  ratingModal.classList.remove('active');
+});
 
-memberBtn.onclick = () => {
+memberButton.addEventListener('click', () => {
   loginModal.classList.remove('active');
   memberModal.classList.toggle('active');
   sellerModal.classList.remove('active');
-  cateModal.classList.remove('active');
-}
+  categoriesModal.classList.remove('active');
+  ratingModal.classList.remove('active');
+});
 
-sellerBtn.onclick = () => {
-  loginModal.classList.remove('active');
-  memberModal.classList.remove('active');
+sellerButton.addEventListener('click', () => {
   sellerModal.classList.toggle('active');
-  cateModal.classList.remove('active');
-}
+  loginModal.classList.remove('active');
+  memberModal.classList.remove('active');
+  categoriesModal.classList.remove('active');
+  ratingModal.classList.remove('active');
+});
 
-cateBtn.onclick = () => {
-  cateToggle.classList.toggle('fa-angle-down');
+ratingButton.addEventListener('click', () => {
+  sellerModal.classList.remove('active');
+  loginModal.classList.remove('active');
+  memberModal.classList.remove('active');
+  categoriesModal.classList.remove('active');
+  ratingModal.classList.toggle('active');
+});
+
+categoriesButton.addEventListener('click', () => {
+  categoriesToggle.classList.toggle('fa-angle-up');
+  categoriesToggle.classList.toggle('fa-angle-down');
   loginModal.classList.remove('active');
   memberModal.classList.remove('active');
   sellerModal.classList.remove('active');
-  cateModal.classList.toggle('active');
+  categoriesModal.classList.toggle('active');
   toggleColor.classList.toggle('active');
-}
-
-userPopBtn.onclick = () => {
-  loginModal.classList.remove('active');
-}
-
-memberExitBtn.onclick = () => {
-  memberModal.classList.remove('active');
-}
-
-sellerExitBtn.onclick = () => {
-  sellerModal.classList.remove('active');
-}
-
-registerExitBtn.onclick = () => {
-  registerModal.classList.remove('active');
-}
-
-// Filters
-priceBtn.onclick = () => {
-  priceToggle.classList.toggle('fa-angle-up');
-  priceModal.classList.toggle('active');
-}
-
-colorBtn.onclick = () => {
-  colorToggle.classList.toggle('fa-angle-up');
-  colorModal.classList.toggle('active');
-}
-
-sizeBtn.onclick = () => {
-  sizeToggle.classList.toggle('fa-angle-up');
-  sizeModal.classList.toggle('active');
-}
+});
